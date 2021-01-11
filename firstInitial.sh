dd273366295cc048d61417c7cae8e1e1e1b82c56
iiot-tw <32183809+iiot-tw@users.noreply.github.com>
2021-01-11 17:58:47 +0800

    Create firstInitial.sh

---
+++ firstInitial.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+if ! id | grep -q root; then
+        echo "must be run as root"
+        exit
+fi
+
+if [ "x$1" == "x" ] || [ "x$2" == "x" ]; then
+  echo "Usage: $0 {thingsboard_host} {thingsboard_token}"
+  exit 1
+fi
+
+/neousys/eth0dhcp.sh
+/opt/source/docker/initIgtNodered.sh
+/opt/source/tbgateway/initTbGateway.sh $1 $2
+systemctl enable thingsboard-gateway.service
+#systemctl start thingsboard-gateway.service
+#sleep 10
+#systemctl restart thingsboard-gateway.service
+reboot
